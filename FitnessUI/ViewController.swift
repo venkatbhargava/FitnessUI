525b03c996020c59a91bafdadaeb0ad251899663
Venkata Bhargav <bhargav.venkata.a@gmail.com>
2020-06-15 18:36:12 +0530

    Initial Commit

---
+++ FitnessUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FitnessUI
+//
+//  Created by Venkata Bhargav on 15/06/20.
+//  Copyright © 2020 venkat bhargav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
